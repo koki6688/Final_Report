f41c37f3f9e232cae9ab3d1854530aa2b727d0cd
joehsiao <joehsiao@intumit.com>
2019-07-05 14:47:51 +0800

    小優化與微調

--- routes/webRouters.js
+++ routes/webRouters.js
@@ -18,23 +18,29 @@ router.get('/customer', (req, res, next) => {
 });
 
 /* GET constituent page. */
-router.get('/constituent', (req, res, next) => {
-    dataset.getConstituent().then(results => {
-        let select_date = results.filter(result => {
+router.get('/constituent', async (req, res, next) => {
+    try {
+        const constituent = await dataset.getConstituent();
+        const select_date = constituent.filter(result => {
             return result[0] === `2012/12/19`;
         });
         res.render('constituent', {result: select_date, range: `2012/12/19~2013/06/19`});
-    });
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 /* POST constituent page. */
-router.post('/constituent', (req, res, next) => {
-    dataset.getConstituent().then(results => {
-        let select_date = results.filter(result => {
+router.post('/constituent', async (req, res, next) => {
+    try {
+        const constituent = await dataset.getConstituent();
+        const select_date = constituent.filter(result => {
             return `${req.body.date}`.substring(0, 10) === result[0];
         });
         res.render('constituent', {result: select_date, range: req.body.range});
-    });
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 /* GET performance page. */
@@ -64,53 +70,74 @@ router.get('/implementation', (req, res, next) => {
 
 
 /* GET index graph data */
-router.get('/indexData', (req, res, next) => {
-    Promise.all([dataset.getDate(), dataset.getIndex()]).then(value => {
-        res.send({date: value[0], index: value[1]});
-    });
+router.get('/indexData', async (req, res, next) => {
+    try {
+        const [date, index] = await Promise.all([dataset.getDate(), dataset.getIndex()]);
+        res.send({date, index});
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 /* GET index performance graph data */
-router.get('/indexPerformanceData', (req, res, next) => {
-    Promise.all([dataset.getDate(), dataset.getIndex(), dataset.getTM100Performance(), dataset.getTF001Performance()]).then(value => {
-        res.send({date: value[0], index: [value[1], value[2], value[3]]});
-    });
+router.get('/indexPerformanceData', async (req, res, next) => {
+    try {
+        const [date, index, tm100, tf001] = await Promise.all([dataset.getDate(), dataset.getIndex(), dataset.getTM100Performance(), dataset.getTF001Performance()]);
+        res.send({date, index: [index, tm100, tf001]});
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 /* GET performance graph data */
-router.get('/performanceData', (req, res, next) => {
-    dataset.getPerformance().then(performance => {
-        res.send({performance: performance});
-    });
+router.get('/performanceData', async (req, res, next) => {
+    try {
+        const performance = await dataset.getPerformance();
+        res.send({performance});
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 /* GET performance 1y graph data */
-router.get('/performanceData_1y', (req, res, next) => {
-    dataset.getPerformance1y().then(performance => {
-        res.send({performance: performance});
-    });
+router.get('/performanceData_1y', async (req, res, next) => {
+    try {
+        const performance = await dataset.getPerformance1y();
+        res.send({performance});
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 
 /* GET performance graph data */
-router.get('/performanceData_3y', (req, res, next) => {
-    dataset.getPerformance3y().then(performance => {
-        res.send({performance: performance});
-    });
+router.get('/performanceData_3y', async (req, res, next) => {
+    try {
+        const performance = await dataset.getPerformance3y();
+        res.send({performance});
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 /* GET implementation cumulative compensation graph data */
-router.get('/cumulativeCompensation', (req, res, next) => {
-    Promise.all([dataset.getDate(), dataset.getImplementation()]).then(value => {
-        res.send({date: value[0], cumulativeCompensation: value[1]});
-    });
+router.get('/cumulativeCompensation', async (req, res, next) => {
+    try {
+        const [date, cumulativeCompensation] = await Promise.all([dataset.getDate(), dataset.getImplementation()]);
+        res.send({date, cumulativeCompensation});
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 /* GET implementation performance compare graph data */
-router.get('/implementation_performance', (req, res, next) => {
-    dataset.getImplementationPerformance().then(performance => {
-        res.send({performance: performance});
-    });
+router.get('/implementation_performance', async (req, res, next) => {
+    try {
+        const performance = await dataset.getImplementationPerformance();
+        res.send({performance});
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 module.exports = router;
